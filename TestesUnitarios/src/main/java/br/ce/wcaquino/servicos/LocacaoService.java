3f5ef4967ad409360a3ccff20441bb6c2afa99bc
Maurício Farias <mauricio.farias@palmsoft.com.br>
2021-07-15 22:24:16 -0300

    Added manual testing, not using unitary testing

---
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -0,0 +1,46 @@
+package br.ce.wcaquino.servicos;
+
+import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
+
+import java.util.Date;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
+
+public class LocacaoService {
+	
+	public Locacao alugarFilme(Usuario usuario, Filme filme) {
+		var locacao = new Locacao();
+		locacao.setFilme(filme);
+		locacao.setUsuario(usuario);
+		locacao.setDataLocacao(new Date());
+		locacao.setValor(filme.getPrecoLocacao());
+
+		//Entrega no dia seguinte
+		var dataEntrega = new Date();
+		dataEntrega = adicionarDias(dataEntrega, 1);
+		locacao.setDataRetorno(dataEntrega);
+		
+		//Salvando a locacao...	
+		//TODO adicionar mÃ©todo para salvar
+		
+		return locacao;
+	}
+
+	public static void main(String[] args) {
+		//cenario
+		var service = new LocacaoService();
+		var usuario = new Usuario("Usuario 1");
+		var filme = new Filme("Filme 1", 2, 5.0);
+		
+		//ação
+		var locacao = service.alugarFilme(usuario, filme);
+		
+		//verificação
+		System.out.println(locacao.getValor() == 5);
+		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+	}
+}
