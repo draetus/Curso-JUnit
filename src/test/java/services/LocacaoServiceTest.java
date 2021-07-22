470fbacc53abf9d5e3cd81cc3902b7692e887665
Maurício Farias <mauricio.farias@palmsoft.com.br>
2021-07-21 21:23:46 -0300

    Adicionado ErrorCollector a classe de testes

--- src/test/java/services/LocacaoServiceTest.java
+++ src/test/java/services/LocacaoServiceTest.java
@@ -2,22 +2,25 @@ package services;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.junit.Assert.assertThat;
 import static utils.DataUtils.isMesmaData;
 import static utils.DataUtils.obterDataComDiferencaDias;
 
 import java.util.Date;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import model.Filme;
 import model.Usuario;
 
 public class LocacaoServiceTest {
 	
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+	
 	@Test
-	public void teste() {
+	public void testeLocacao() {
 		//cenario
 		var service = new LocacaoService();
 		var usuario = new Usuario("Usuario 1");
@@ -27,10 +30,11 @@ public class LocacaoServiceTest {
 		var locacao = service.alugarFilme(usuario, filme);
 		
 		//verificação
-		assertThat(locacao.getValor(), is(equalTo(5.0)));
-		assertThat(locacao.getValor(), is(not(6.0)));
-		assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
-		assertThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
+		error.checkThat(locacao.getValor(), is(equalTo(5.0)));
+		error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		error.checkThat(isMesmaData(locacao.getDataRetorno(), obterDataComDiferencaDias(1)), is(true));
 	}
+	
+	
 
 }
